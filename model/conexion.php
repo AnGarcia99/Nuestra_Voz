9d27ff70c15b98a70c88cb37257b6a3cbcf704cc
AnGarcia99 <86631267+AnGarcia99@users.noreply.github.com>
2021-06-29 00:12:53 -0500

    Update conexion.php

--- model/conexion.php
+++ model/conexion.php
@@ -4,7 +4,7 @@ class Conexion {
 
     static public function conectar() {
 
-        $link = new PDO('mysql:host=bldppo0ujxpfsb4eobju-mysql.services.clever-cloud.com:3306;dbname=bldppo0ujxpfsb4eobju', 'ujynbvhqif25hyig', 'ujynbvhqif25hyig');
+        $link = new PDO('mysql:host=bldppo0ujxpfsb4eobju-mysql.services.clever-cloud.com;dbname=bldppo0ujxpfsb4eobju', 'ujynbvhqif25hyig', 'WLZXQeBlksFBrHPUghXD');
         $link -> exec('set names utf8');
 
         return $link;
