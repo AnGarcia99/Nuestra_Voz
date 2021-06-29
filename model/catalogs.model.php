b92e910118f9bce3685094de4c34014249af1978
AnGarcia99 <86631267+AnGarcia99@users.noreply.github.com>
2021-06-29 04:56:02 -0500

    Update catalogs.model.php

--- model/catalogs.model.php
+++ model/catalogs.model.php
@@ -40,10 +40,7 @@ class ModelCatalogs {
 
     /** Add a new Description in a Catalog */
     public static function addDescription($nameCatalog, $Cv, $Ds, $Description) {
-        $sql = "INSERT INTO $nameCatalog($Ds) VALUES(:$Ds);
-                SET @num := 0;
-                UPDATE $nameCatalog SET $ColCv = @num := (@num+1);
-                ALTER TABLE $nameCatalog AUTO_INCREMENT = 1";
+        $sql = "INSERT INTO $nameCatalog($Ds) VALUES(:$Ds)";
 
         $stmt = Conexion::conectar() -> prepare($sql);
         $stmt -> bindParam(":$Ds", $Description, PDO::PARAM_STR);
